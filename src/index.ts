2b5fbf6c26106a485a3dd0faba456ebabf73728e
Scorpi-ON <s.scorpi-on@ya.ru>
2024-09-16 23:23:56 +0300

    Настроил выделение жирным типа шифрования

--- src/index.ts
+++ src/index.ts
@@ -19,16 +19,22 @@ window.addEventListener('DOMContentLoaded', () => {
     encryptedTextarea.addEventListener('change', () => onTextChange('encrypted'));
     encryptedTextarea.addEventListener('keypress', () => onTextChange('encrypted'));
 
-    const richelieuRadio: HTMLElement = document.getElementById('cipherMethodRadio_Richelieu')!;
-    const feistelRadio: HTMLElement = document.getElementById('cipherMethodRadio_Feistel')!;
+    const richelieuRadio: HTMLElement = document.getElementById('cipherMethodRadioRichelieu')!;
+    const feistelRadio: HTMLElement = document.getElementById('cipherMethodRadioFeistel')!;
     const richelieuInfo: HTMLElement = document.getElementById('richelieuInfo')!;
     const feistelInfo: HTMLElement = document.getElementById('feistelInfo')!;
+    const richelieuLabel: HTMLElement = document.getElementById('cipherMethodRadioRichelieuLabel')!;
+    const feistelLabel: HTMLElement = document.getElementById('cipherMethodRadioFeistelLabel')!;
     richelieuRadio.addEventListener('change', () => {
         feistelInfo.classList.add('d-none');
         richelieuInfo.classList.remove('d-none');
+        feistelLabel.classList.remove('fw-bold');
+        richelieuLabel.classList.add('fw-bold');
     });
     feistelRadio.addEventListener('change', () => {
         richelieuInfo.classList.add('d-none');
         feistelInfo.classList.remove('d-none');
+        richelieuLabel.classList.remove('fw-bold');
+        feistelLabel.classList.add('fw-bold');
     });
 });
