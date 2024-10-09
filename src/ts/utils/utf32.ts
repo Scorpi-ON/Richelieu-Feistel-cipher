eafe0af473b1972519c67a76f47d40501c75d062
Scorpi-ON <s.scorpi-on@ya.ru>
2024-10-09 12:27:32 +0300

    Добавил функции работы с base64

--- src/ts/utils/utf32.ts
+++ src/ts/utils/utf32.ts
@@ -16,9 +16,27 @@ function decode(encoded: Uint8Array): string {
     return bufView.reduce((result, c) => result + String.fromCharCode(c), '');
 }
 
+function base64Encode(text: string): string {
+    const utf8Array = encode(text);
+    const utf8String = utf8Array.reduce((str, byte) => str + String.fromCharCode(byte), '');
+    return window.btoa(utf8String);
+}
+
+function base64Decode(encodedText: string): string {
+    const base64String = window.atob(encodedText);
+    const len = base64String.length;
+    const utf8Array = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        utf8Array[i] = base64String.charCodeAt(i);
+    }
+    return decode(utf8Array);
+}
+
 export default {
     encode,
     decode,
+    base64Encode,
+    base64Decode,
     NAME,
     WHITESPACE_CODES,
     BYTES_PER_ELEMENT,
